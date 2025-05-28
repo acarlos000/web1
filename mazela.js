a3203acca2776487bfe8ba21ac7abd19a6e94489
acarlos000 <antoincarlu050@gmail.com>
2025-05-28 17:48:20 -0300

    Add files via upload

---
+++ mazela.js
@@ -0,0 +1,14 @@
+function somar(){
+    var ipt_X;
+    ipt_X = document.getElementById("iptX");
+    var x = parseFloat(ipt_X.value);
+
+    var y = parseFloat(document.getElementById("iptY").value);
+
+    var spnRes = document.getElementById("spnRes");
+
+    var res = x + y;
+
+    alert(res);
+    
+}
